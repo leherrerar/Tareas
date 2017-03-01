7880df33e62fb01daeb5d83cfb2cc131f05bd80d
leherrerar <leherrerar@unal.edu.co>
2017-02-28 23:05:10 -0500

    progmrma de euler  #7

---
+++ euler.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include <iomanip> 
+using namespace std;
+const int N = 5000;
+
+
+double sumaup(int N){
+   double sum=0;	
+   for (int n=1; n<=N;n++){
+	sum=sum+(1.0/n);
+    }
+   return sum;
+}
+
+
+
+double sumadown( int N){
+	double sum=0;	
+	for (int n=N; n>=1;n--){
+		sum=sum+(1.0/n);
+	}
+	return sum;
+}
+
+
+
+int main(){
+	double gamma = sumadown(N)- log(N);	
+	printf(" %2.16e  \n", gamma );
+  
+
+}
